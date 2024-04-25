4f1cbb8098c6efdacf369b98c18dc933654e4bdb
jervitha <147043738+jervitha@users.noreply.github.com>
2024-04-26 02:46:55 +0530

    sound service implemented in game service

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using ServiceLocator.Utilities;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
     [SerializeField] public PlayerScriptableObject playerScriptableObject;
-    
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
+
 
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject,audioEffects,backgroundMusic);
     }
 
    public void Update()
